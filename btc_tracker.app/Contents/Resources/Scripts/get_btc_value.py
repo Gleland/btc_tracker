9a5d91209e6873486f5bbfb1135847aa5393f6f5
GThompson <garrett.leland.thompson@gmail.com>
2018-02-11 19:50:18 -0800

    removed comments

--- btc_tracker.app/Contents/Resources/Scripts/get_btc_value.py
+++ btc_tracker.app/Contents/Resources/Scripts/get_btc_value.py
@@ -10,10 +10,3 @@ raw_data = urlopen(link)
 json_data = json.load(raw_data)
 price = str(json_data["USD"]["last"])
 print("$"+str(price))
-#os.system('osascript btc.app {}'.format(price))
-# os.system('open ./btc.app --args {}'.format(price))
-#os.system("""osascript -e 'display notification {} with title "BTC Tracker"subtitle (current date) as string'""".format(price,"btc.icns"))
-#os.system('open -e btc.app {}'.format(price))
-
-
-#os.system('osascript -e "display dialog {} with title {} with icon POSIX file {}"'.format(price,"BTC Tracker","BTC.png"))
