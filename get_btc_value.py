cc17112ed0398aece2cb7abad91fbb44256d51cc
GThompson <garrett.leland.thompson@gmail.com>
2018-03-11 22:00:18 -0700

    updated version

--- get_btc_value.py
+++ get_btc_value.py
@@ -3,10 +3,9 @@
 
 import json
 from urllib.request import urlopen
-import os
 
 link = "https://blockchain.info/ticker"
 raw_data = urlopen(link)
 json_data = json.load(raw_data)
 price = str(json_data["USD"]["last"])
-print("$"+str(price))
+print("$"+price)
